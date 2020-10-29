f9a7efd5679bed86d47831a56a33430e6a9d9967
Wenfei Zhou <wzhou2046@gmail.com>
2020-10-28 20:47:20 -0400

    try to figure out how to deploy node cluster...

--- server/servers.js
+++ server/servers.js
@@ -1,1 +1,4 @@
 //Socket IO server that will serve node and react clients
+//Entry point for node cluster which will make workers and the workers will do the socket.io handling
+const cluster = require('cluster');
+const http = require('http');
